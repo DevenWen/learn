83e43d1368a21c1a34c089a15fcefc0f452e9228
刘园园 <liuyuanyuan@tungee.com>
2020-05-01 18:43:45 +0800

    提交 Java SPI 机制的 Demo

---
+++ base/src/main/java/com/qpm/learn/spi/Bumblebee.java
@@ -0,0 +1,12 @@
+package com.qpm.learn.spi;
+
+/**
+ * @Author kangqiang.w
+ * @Date 2020/5/1
+ */
+public class Bumblebee implements Robot{
+    @Override
+    public void sayHello() {
+        System.out.println("Hello, I am Bumblebee");
+    }
+}
---
+++ base/src/main/java/com/qpm/learn/spi/Robot.java
@@ -0,0 +1,13 @@
+package com.qpm.learn.spi;
+
+/**
+ * Java SPI Interface Demo from
+ * http://dubbo.apache.org/zh-cn/docs/source_code_guide/dubbo-spi.html
+ *
+ * @Author kangqiang.w
+ * @Date 2020/5/1
+ */
+public interface Robot {
+
+    void sayHello();
+}
