975013f523d12ee141203f4b4294cc3ce74ed488
qpm <kangqiang.w@gmail.com>
2020-08-06 17:39:09 +0800

    refactor:重建 POM 依赖关系

--- tool/src/main/java/com/qpm/learn/DemoApplication.java
+++ tool/src/main/java/com/qpm/learn/DemoApplication.java
@@ -1,14 +1,15 @@
 package com.qpm.learn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
-@SpringBoo
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class DemoApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
